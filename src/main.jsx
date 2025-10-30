d91d8b1d7722b79eb3d969a7e976817f76b093a6
Naveen AN <naveen.an0523@gmail.com>
2025-10-31 01:18:03 +0530

    Login Page

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import SessionLanding from "./components/SessionLanding";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<SessionLanding />} />
         <Route path="/session/:sessionId" element={<App />} />
       </Routes>
     </BrowserRouter>
